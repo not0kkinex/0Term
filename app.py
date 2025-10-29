b5f58f463c18028291d421094857b4435d5e56ac
not0kkinex <0kki3169@gmail.com>
2025-10-29 10:54:15 +0300

    Rename main.py to app.py and update comments

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-# main.py
+# app.py
 from gui.terminal_ui import TerminalUI
 
 if __name__ == "__main__":
-    # Deneyebileceğin temalar: "Dark", "Light", "Dracula"
+    # You Can Try These Themes: "Dark", "Light", "Dracula"
     app = TerminalUI(theme_name="Dracula") 
     app.mainloop()
