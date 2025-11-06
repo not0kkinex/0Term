676a9c5855caed0ef2ae789b55515e6b2a1e81c5
0kki <0kki3169@gmail.com>
2025-11-06 20:57:28 +0300

    Delete 0Term directory

--- 0Term/app.py
+++
@@ -1,7 +0,0 @@
-# app.py
-from gui.terminal_ui import TerminalUI
-
-if __name__ == "__main__":
-    # You Can Try These Themes: "Dark", "Light", "Dracula"
-    app = TerminalUI(theme_name="Dracula") 
-    app.mainloop()
